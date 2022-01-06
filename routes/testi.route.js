c3578840eb6f9f44d42be66122f0b8baccde7ba0
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2022-01-06 19:11:11 +0800

    change route testimoni & pengalaman pengguna

--- routes/testi.route.js
+++ routes/testi.route.js
@@ -2,9 +2,9 @@ const router = require('express').Router()
 const { TestimoniController, AuthController } = require('../controllers')
 
 // admin only
-router.patch('/edit-testimoni/:id', AuthController.authenticationJWT, TestimoniController.updateTestimoni)
-router.delete('/delete-testimoni/:id', AuthController.authenticationJWT, TestimoniController.deleteTestimoni)
-router.post('/add-testimoni',  AuthController.authenticationJWT, TestimoniController.postTestimoni)
+router.patch('/edit-pengalaman/:id', AuthController.authenticationJWT, TestimoniController.updateTestimoni)
+router.delete('/delete-pengalaman/:id', AuthController.authenticationJWT, TestimoniController.deleteTestimoni)
+router.post('/add-pengalaman',  AuthController.authenticationJWT, TestimoniController.postTestimoni)
 
 // admin, user, and non user
 router.get('/:id', TestimoniController.getTestimoniById)
--- routes/testiPsikolog.route.js
+++ routes/testiPsikolog.route.js
@@ -2,9 +2,9 @@ const router = require('express').Router()
 const { TestiPsikologController, AuthController } = require('../controllers')
 
 // admin only
-router.patch('/edit-testiPsikolog/:id', AuthController.authenticationJWT, TestiPsikologController.updateTestiPsikolog)
-router.delete('/delete-testiPsikolog/:id', AuthController.authenticationJWT, TestiPsikologController.deleteTestiPsikolog)
-router.post('/add-testiPsikolog',  AuthController.authenticationJWT, TestiPsikologController.postTestiPsikolog)
+router.patch('/edit-testimoni/:id', AuthController.authenticationJWT, TestiPsikologController.updateTestiPsikolog)
+router.delete('/delete-testimoni/:id', AuthController.authenticationJWT, TestiPsikologController.deleteTestiPsikolog)
+router.post('/add-testimoni',  AuthController.authenticationJWT, TestiPsikologController.postTestiPsikolog)
 
 // admin, user, psikolog and non user
 router.get('/:id', TestiPsikologController.getTestiPsikologById)
