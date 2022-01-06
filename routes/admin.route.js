e13ad6488585cec1380408ef89d7d5f53062821f
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2022-01-06 12:40:12 +0800

    add route password

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -6,6 +6,7 @@ router.post('/add-users', AuthController.authenticationJWT, AdminController.addU
 router.get('/list-users', AuthController.authenticationJWT, AdminController.getUsers)
 router.get('/users/:id', AuthController.authenticationJWT, AdminController.getUserById)
 router.patch('/users/:id', AuthController.authenticationJWT, AdminController.updateUser)
+router.patch('/users-password/:id', AuthController.authenticationJWT, AdminController.updatePassword)
 router.delete('/users/:id', AuthController.authenticationJWT, AdminController.deleteUser)
 
 module.exports = router
